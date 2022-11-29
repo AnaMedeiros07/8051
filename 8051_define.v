b27426aeb58c2a8c2a627502842bf766773f449c
FrSalgado <64605235+FrSalgado@users.noreply.github.com>
2022-11-29 13:14:40 +0000

    Added Alu_Core and define with apu_op_codes

---
+++ 8051_define.v
@@ -0,0 +1,40 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 11/25/2022 11:04:04 AM
+// Design Name: 
+// Module Name: 8051_define
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+// - Define codes for ALU
+
+`define ALU_INC 4'b 0000
+`define ALU_DEC 4'b 0001
+`define ALU_ADD 4'b 0010
+`define ALU_ADDC 4'b 0011
+`define ALU_SUBB 4'b 0100
+`define ALU_MUL 4'b 0101
+`define ALU_DIV 4'b 0110
+`define ALU_RR 4'b 0111
+`define ALU_RRC 4'b 1000
+`define ALU_RL 4'b 1001
+`define ALU_RLC 4'b 1010
+`define ALU_CPL 4'b 1011
+`define ALU_DA 4'b 1100
+`define ALU_SWAP 4'b 1101
+
+
+
