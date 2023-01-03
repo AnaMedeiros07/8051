a6474954ab5c0bf3d727c5b304ca88b3442acb28
FrSalgado <64605235+FrSalgado@users.noreply.github.com>
2023-01-03 15:36:16 +0000

    Update psw.v

--- psw.v
+++ psw.v
@@ -23,6 +23,8 @@
 `define CY_SET          2'b01
 `define CY_OV_SET       2'b10
 `define CY_OV_AC_SET    2'b11
+`define PSW_SFR_ADDR    8'hD0
+`define PSW_SFR_B_ADDR  5'b11010
 
 module psw(
     input clock,
@@ -31,12 +33,11 @@ module psw(
     input carry_in,
     input aux_carry_in,
     input overflow_in,
-    input [7:1] data_in,
+    input [7:0] data_in,
     input [7:0] acc_in,
-    
+    input [7:0]addr,
     input write_en,
     input write_bit_en,
-    input [2:0] bit_addr,
     
     input [1:0] flag_set,
      
@@ -55,42 +56,45 @@ module psw(
     */
     
     assign psw_data = data;
-    
-always @(posedge reset)
-begin
-    data = 8'h00;
-end
 
-always @(posedge clock)
+always @(posedge clock or posedge reset)
 begin
-    if(write_en & !write_bit_en)
-        data [7:1] = data_in [7:1];
-    else if(write_en & write_bit_en)
-        data[bit_addr[2:0]]= carry_in;
-    else begin
-        case (flag_set)
+    if(reset)
+        data= 8'h00;
+    else begin 
+        if(write_en & !write_bit_en & (addr == `PSW_SFR_ADDR))
+            data [7:1] = data_in [7:1];
             
-            `CY_SET:
-                begin
-                    data[7] = carry_in;
-                end
+        else if(write_en & write_bit_en & (addr[7:3] == `PSW_SFR_B_ADDR))
+            data[addr[2:0]]= carry_in;
             
-            `CY_OV_SET:
-                begin
-                    data[7] = carry_in;
-                    data[2] = overflow_in;
-                end
-            
-            `CY_OV_AC_SET:  
-                begin
-                    data[7] = carry_in;
-                    data[2] = overflow_in;
-                    data[6] = aux_carry_in;
-                end
-        endcase
-    end
+        else begin
+            case (flag_set)
+                
+                `CY_SET:
+                    begin
+                        data[7] = carry_in;
+                    end
+                
+                `CY_OV_SET:
+                    begin
+                        data[7] = carry_in;
+                        data[2] = overflow_in;
+                    end
+                
+                `CY_OV_AC_SET:  
+                    begin
+                        data[7] = carry_in;
+                        data[2] = overflow_in;
+                        data[6] = aux_carry_in;
+                    end
+            endcase
+        end
     
     data[0]= ^acc_in; //parity update
+    end
+    
+    
 end
 
 
