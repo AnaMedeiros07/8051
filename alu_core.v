6b68b631f6777c74d31dc98ea170724b536373fe
FrSalgado <64605235+FrSalgado@users.noreply.github.com>
2023-01-03 15:35:49 +0000

    Update alu_core.v

--- alu_core.v
+++ alu_core.v
@@ -51,8 +51,12 @@ initial begin
 
 end 
 
-always @(posedge reset)
+
+
+always @(alu_opcode or posedge reset)
 begin
+
+if(reset) begin
     overflow_out = 0;
     aux_carry_out = 0;
     carry_out = 0;
@@ -60,125 +64,123 @@ begin
     op_out_1=8'h00;
     op_out_2=8'h00;
 end
-
-always @(posedge clock)
-begin
-
-case (alu_opcode)
-    
-    `ALU_INC: begin
-    
-        op_out_1 = op_in_1 + 1;
-    
-    end
-    
-    `ALU_DEC: begin
-    
-        op_out_1 = op_in_1 - 1;
-    
-    end
-    
-    `ALU_ADD: begin
+else begin       
+    case (alu_opcode)
         
-        {aux_carry_out,op_out_1[3:0]} = op_in_1[3:0] + op_in_2[3:0];
-        {carry_out,op_out_1[7:4]}= op_in_1[7:4] + op_in_2[7:4] + aux_carry_out;
-        overflow_out = op_out_1[7]^op_out_1[6];
-    end
-    
-    `ALU_ADDC: begin
+        `ALU_INC: begin
         
-        {aux_carry_out,op_out_1[3:0]} = op_in_1[3:0] + op_in_2[3:0] + carry_in;
-        {carry_out,op_out_1[7:4]}= op_in_1[7:4] + op_in_2[7:4] + aux_carry_out;
-        overflow_out = op_out_1[7]^op_out_1[6];
-    end
-    
-    `ALU_SUBB: begin
-        {aux_carry_out, op_out_1[3:0]} = op_in_1[3:0] - op_in_2[3:0];
-        {carry_out, op_out_1[7:4]}= op_in_1[7:4] - op_in_2[7:4] - aux_carry_out;
-        overflow_out = op_out_1[7]^op_out_1[6];         
+            {op_out_2,op_out_1} = {op_in_2,op_in_1} + 1;
+            
+        end
         
-    end
-    
-    `ALU_MUL: begin
-        {op_out_2,op_out_1} = op_in_1 * op_in_2;
-        carry_out = 0;
-        overflow_out = (op_in_2 != 0) ? 1'b1 : 1'b0;
-    end
-    
-    `ALU_DIV: begin
-        op_out_1 = op_in_1 / op_in_2;
-        op_out_2 = op_in_1 % op_in_2;
-        carry_out = 0;
-        overflow_out = (op_in_2 == 0) ? 1'b1 : 1'b0;
-    
-    end
-    
-    `ALU_RR: begin
-        op_out_1 = {op_in_1[0],op_in_1[7:1]};
-    
-    end
-    
-    `ALU_RRC: begin
-        carry_out = op_in_1[0];
-        op_out_1 = {carry_in,op_in_1[7:1]}; 
-    
-    end
-    
-    `ALU_RL: begin
-        op_out_1 = {op_in_1[6:0],op_in_1[7]};
-    
-    end
-    
-    `ALU_RLC: begin
-        carry_out = op_in_1[7];
-        op_out_1 = {op_in_1[6:0],carry_in}; 
-    
-    end
-    
-    `ALU_CPL: begin
-        op_out_1=~op_in_1;
-    
-    end
-    
-    `ALU_DA: begin
-       
-        //op_1 = (aux_carry==1 || op_1[3:0]>9) op_1 + 8'h06 : op_1;
-        if(aux_carry_in==1 || op_in_1[3:0]>4'd9) op_out_1 = op_in_1 + 8'h06;
+        `ALU_DEC: begin
         
-        //op_1 = ((carry==1) || (op_1[7:4]>4'd9 )) op_1 + 8'h60 : op_1;
-        if(carry_in==1 || op_in_1[7:4]>4'd9) op_out_1 = op_in_1 + 8'h60;
+            op_out_1 = op_in_1 - 1;
         
-    end
-    
-    `ALU_SWAP: begin
-    
-        op_out_1 = {op_in_1[3:0],op_in_1[7:4]};
+        end
         
-    end
-    
-    `ALU_ORL: begin
+        `ALU_ADD: begin
+            
+            {aux_carry_out,op_out_1[3:0]} = op_in_1[3:0] + op_in_2[3:0];
+            {carry_out,op_out_1[7:4]}= op_in_1[7:4] + op_in_2[7:4] + aux_carry_out;
+            overflow_out = op_out_1[7]^op_out_1[6];
+        end
         
-        op_out_1 = op_in_1 | op_in_2;
-        carry_out = carry_in | bit_in;
+        `ALU_ADDC: begin
+            
+            {aux_carry_out,op_out_1[3:0]} = op_in_1[3:0] + op_in_2[3:0] + carry_in;
+            {carry_out,op_out_1[7:4]}= op_in_1[7:4] + op_in_2[7:4] + aux_carry_out;
+            overflow_out = op_out_1[7]^op_out_1[6];
+        end
         
-    end
-    
-    `ALU_XRL: begin
+        `ALU_SUBB: begin
+            {aux_carry_out, op_out_1[3:0]} = op_in_1[3:0] - op_in_2[3:0];
+            {carry_out, op_out_1[7:4]}= op_in_1[7:4] - op_in_2[7:4] - aux_carry_out;
+            overflow_out = op_out_1[7]^op_out_1[6];         
+            
+        end
         
-        op_out_1 = op_in_1 | op_in_2;
-    
-    end
-    
-    `ALU_ANL: begin
+        `ALU_MUL: begin
+            {op_out_2,op_out_1} = op_in_1 * op_in_2;
+            carry_out = 0;
+            overflow_out = (op_in_2 != 0) ? 1'b1 : 1'b0;
+        end
         
-        op_out_1 = op_in_1 | op_in_2;
-        carry_out = carry_in | bit_in;
+        `ALU_DIV: begin
+            op_out_1 = op_in_1 / op_in_2;
+            op_out_2 = op_in_1 % op_in_2;
+            carry_out = 0;
+            overflow_out = (op_in_2 == 0) ? 1'b1 : 1'b0;
         
-    end
-    
-endcase
-
+        end
+        
+        `ALU_RR: begin
+            op_out_1 = {op_in_1[0],op_in_1[7:1]};
+        
+        end
+        
+        `ALU_RRC: begin
+            carry_out = op_in_1[0];
+            op_out_1 = {carry_in,op_in_1[7:1]}; 
+        
+        end
+        
+        `ALU_RL: begin
+            op_out_1 = {op_in_1[6:0],op_in_1[7]};
+        
+        end
+        
+        `ALU_RLC: begin
+            carry_out = op_in_1[7];
+            op_out_1 = {op_in_1[6:0],carry_in}; 
+        
+        end
+        
+        `ALU_CPL: begin
+            op_out_1=~op_in_1;
+        
+        end
+        
+        `ALU_DA: begin
+           
+            //op_1 = (aux_carry==1 || op_1[3:0]>9) op_1 + 8'h06 : op_1;
+            if(aux_carry_in==1 || op_in_1[3:0]>4'd9) op_out_1 = op_in_1 + 8'h06;
+            
+            //op_1 = ((carry==1) || (op_1[7:4]>4'd9 )) op_1 + 8'h60 : op_1;
+            if(carry_in==1 || op_in_1[7:4]>4'd9) op_out_1 = op_in_1 + 8'h60;
+            
+        end
+        
+        `ALU_SWAP: begin
+        
+            op_out_1 = {op_in_1[3:0],op_in_1[7:4]};
+            
+        end
+        
+        `ALU_ORL: begin
+            
+            op_out_1 = op_in_1 | op_in_2;
+            carry_out = carry_in | bit_in;
+            
+        end
+        
+        `ALU_XRL: begin
+            
+            op_out_1 = op_in_1 | op_in_2;
+        
+        end
+        
+        `ALU_ANL: begin
+            
+            op_out_1 = op_in_1 | op_in_2;
+            carry_out = carry_in | bit_in;
+            
+        end
+        
+    endcase
+end
 end
 
  
 endmodule
+
